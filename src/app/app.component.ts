de37e98d58698c43ae407432fac8fdd07eabce16
Major Soldier <majorsoldier603@proton.me>
2024-05-24 21:20:43 +0200

    Basic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from "@angular/common";
 import { RouterOutlet } from '@angular/router';
+import { LandingPageComponent } from "./landing-page/landing-page.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, LandingPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
