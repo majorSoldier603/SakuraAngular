90fa09bac80694c58999655527f891fd9e69ba41
Major Soldier <majorsoldier603@proton.me>
2024-05-27 14:11:54 +0200

    Fonts

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,11 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NavbarComponent } from './navbar/navbar.component';
+
 
 @Component({
-  selector: 'app-landing-page',
-  standalone: true,
-  imports: [],
-  templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.scss'
+	selector: 'app-landing-page',
+	standalone: true,
+	imports: [CommonModule, NavbarComponent],
+	template:`
+	<section>
+		<app-navbar></app-navbar>
+		<h1 class="FontRalewayBold ColorWhite">SAKURA RAMEN</h1>
+		<h2 class="FontRalewayRegular ColorWhite">BEST RAMEN IN TOWN</h2>
+	</section>
+	`,
+	styleUrl: './landing-page.component.scss'
 })
 export class LandingPageComponent {
 
